b2ef4d82d1aedbc2e3603e4efcbe6e946ed0ae4c
Marc Høgh <Marchogh@hotmail.com>
2020-06-03 16:52:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+const todoList = document.querySelector('.todo-list');
+const todoForm = document.querySelector('.todo-form');
+const todoItem = document.querySelector('.todo-item');
+const todoButton = document.querySelector('.todo-button');
+const search = document.querySelector('input[name="search-form"]');
+const searchItems = document.querySelectorAll(".todo-item");
+const openTodo = document.querySelector('.open-todo');
+let todoTitle = document.querySelector('input[name="title"]');
+let todoDescription = document.querySelector('input[name="description"]');
+let todoCategory = document.querySelector('select[name="category"]');
+
+
+function createTodo (e) {
+    /* Prevents browser reload */
+    e.preventDefault();
+
+    if (todoTitle.value === '' || todoDescription.value === '') {
+        alert('Title and description cannot be empty');
+        return;
+    }
+
+    todoList.insertAdjacentHTML('afterbegin', `
+     <li class="todo-item">
+                <div class="todo-item-header">
+                    <input type="checkbox" name="checkbox" id="">
+                    <h2 class="todo-title">${todoTitle.value}</h2>
+                    <span class="todo-icon-category-${todoCategory.value}"></span>
+                    <p>${todoCategory.value}</p>
+                </div>
+                <p class="todo-item-body">${todoDescription.value}</p>
+            </li>
+    `);
+    
+    localStorage.setItem(`${todoTitle.value}`, 'test');
+    /* resets form inputs value after submit */
+    todoForm.reset();
+}
+
+todoButton.addEventListener('click', createTodo);
+
+/* Open and closes modal to create a todo */
+openTodo.addEventListener('click', () => {
+    todoForm.classList.toggle('hidden');
+});
+
+/* Search filter */
+search.addEventListener('keyup', function (e) {
+    
+    const term = e.target.value.toLowerCase();
+    
+    Array.from(searchItems).forEach(function (item) {
+        const text = item.textContent.toLowerCase();
+        
+        if (text.includes(term)) {
+            item.style.display = 'block';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    });
+})
