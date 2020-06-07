c72d530c57e27c4e73a28e527e1421357ad9a008
Marc Høgh <Marchogh@hotmail.com>
2020-06-07 17:45:37 +0200

    Fixed bug where search filter would not search for  newly created todos

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const todoList = document.querySelector('.todo-list');
-const todoForm = document.querySelector('.todo-form');
-const todoItem = document.querySelector('.todo-item');
-const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector(".todo-list");
+const todoForm = document.querySelector(".todo-form");
+const todoItem = document.querySelector(".todo-item");
+const todoButton = document.querySelector(".todo-button");
 const search = document.querySelector('input[name="search-form"]');
-const searchItems = document.querySelectorAll(".todo-item");
-const openTodo = document.querySelector('.open-todo');
-const closeTodo = document.querySelector('.create-todo-modal');
+const openTodo = document.querySelector(".open-todo");
+const closeTodo = document.querySelector(".create-todo-modal");
 let todoTitle = document.querySelector('input[name="title"]');
 let todoDescription = document.querySelector('input[name="description"]');
 let todoCategory = document.querySelector('select[name="category"]');
 
+function createTodo(e) {
+  /* Prevents browser reload */
+  e.preventDefault();
 
-function createTodo (e) {
-    /* Prevents browser reload */
-    e.preventDefault();
+  if (todoTitle.value === "" || todoDescription.value === "") {
+    alert("Title and description cannot be empty");
+    return;
+  }
 
-    if (todoTitle.value === '' || todoDescription.value === '') {
-        alert('Title and description cannot be empty');
-        return;
-    }
-
-    todoList.insertAdjacentHTML('afterbegin', `
+  todoList.insertAdjacentHTML(
+    "afterbegin",
+    `
      <li class="todo-item">
                 <div class="todo-item-header">
                     <input type="checkbox" name="checkbox" aria-label="Mark as finished">
@@ -30,46 +30,45 @@ function createTodo (e) {
                 </div>
                 <p class="todo-item-body">${todoDescription.value}</p>
             </li>
-    `);
-    
-    localStorage.setItem(`${todoTitle.value}`, 'test');
-    /* resets form inputs value after submit */
-    todoForm.reset();
+    `
+  );
+
+  localStorage.setItem(`${todoTitle.value}`, "test");
+  /* resets form inputs value after submit */
+  todoForm.reset();
 }
 
-todoButton.addEventListener('click', createTodo);
+todoButton.addEventListener("click", createTodo);
 
 /* Open and closes modal to create a todo */
-openTodo.addEventListener('click', () => {
-    todoForm.classList.toggle('hidden');
+openTodo.addEventListener("click", () => {
+  todoForm.classList.toggle("hidden");
 });
 
 /* Close modal with click outside */
-document.addEventListener('click', function (event) {
-    let isClickInside = todoForm.contains(event.target);
-    let isClickBtn = openTodo.contains(event.target);
-    
-    if (isClickInside || isClickBtn) {
-       return;
-    } else {
-        todoForm.classList.add('hidden');
-    }
-    
+document.addEventListener("click", function (event) {
+  let isClickInside = todoForm.contains(event.target);
+  let isClickBtn = openTodo.contains(event.target);
+
+  if (isClickInside || isClickBtn) {
+    return;
+  } else {
+    todoForm.classList.add("hidden");
+  }
 });
 
 /* Search filter */
-search.addEventListener('keyup', function (e) {
-    
-    const term = e.target.value.toLowerCase();
-    
-    Array.from(searchItems).forEach(function (item) {
-        const text = item.textContent.toLowerCase();
-        
-        if (text.includes(term)) {
-            item.style.display = 'block';
-        }
-        else {
-            item.style.display = 'none';
-        }
-    });
-})
+search.addEventListener("keyup", function (e) {
+  const term = e.target.value.toLowerCase();
+  const newTodoList = todoList.children;
+
+  Array.from(newTodoList).forEach(function (item) {
+    const text = item.textContent.toLowerCase();
+
+    if (text.includes(term)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
